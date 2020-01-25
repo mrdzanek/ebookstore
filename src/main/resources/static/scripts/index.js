dda21cdc2df3b82437d9200ca1709e3fd47f0405
Mirek <rdzanek.miroslaw@gmail.com>
2020-01-25 15:42:30 +0100

    js2

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -8,15 +8,46 @@ function getProducts() {
     .catch((error) => console.log(error))
 }
 
+function toProductHtmlTemplate(product) {
+    return `
+        <li>
+            <h2>${product.title}</h2>
+            <span>${product.description}</span>
+            <img src="${product.cover}"/>
+            <span>${product.price}></span>
+            <button data-product-id="${product.id}">Add basket</button>
+        </li>
+    `;
+}
+
+function toHtmlElement(templateStr) {
+    htmlElement = document.createElement('<div>');
+    htmlElement.innerHTML = templateStr;
+
+    return htmlElement.firstChild();
+}
+
+function initializeAddToBasket(htmlElement) {
+    htmlElement.querySelector('button').addEventListener('click', (e) => {
+        const productId = e.target.getAttribute('data-product-id');
+        console.log(`i am going to add product with id ${product.id}`);
+    });
+
+    return htmlElement;
+}
+
+
 (() => {
     const productsListElement = document.querySelector('.products-list');
 
     getProducts().then((products) => {
         products
-            .map(toHtml)
+            .map(toProductHtmlTemplate)
             .map(toElement)
             .map(initializeAddToBasket)
-            .forEach(appendToProdList)
+            .forEach((element) => {
+                productsListElement.appendChild(element);
+            });
     });
 })();
     console.log("Hello world!")
