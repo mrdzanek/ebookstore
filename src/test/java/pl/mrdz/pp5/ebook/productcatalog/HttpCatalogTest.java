7954071d46c6e8f4fe9c7b9277cfb979ce44082c
Mirek <rdzanek.miroslaw@gmail.com>
2020-01-25 14:30:03 +0100

    testy

--- src/test/java/pl/mrdz/pp5/ebook/productcatalog/HttpCatalogTest.java
+++ src/test/java/pl/mrdz/pp5/ebook/productcatalog/HttpCatalogTest.java
@@ -1,4 +1,40 @@
 package pl.mrdz.pp5.ebook.productcatalog;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
 public class HttpCatalogTest {
+    @Autowired
+    TestRestTemplate http;
+
+    @Autowired
+    ProductCatalogFacade productCatalogFacade;
+
+    @LocalServerPort
+    int port;
+
+    public void itAllowListProductViaHttpEndpoint() {
+        therIsPublishedBook("Lord of the rings");
+        therIsPublishedBook("Hobbit");
+
+        ResponseEntity<Book[]> response = http.getForEntity(
+                String.format("localhost:%s/api/products", port),
+                Book[].class
+        );
+
+        assertThat(response).isEqualTo((HttpStatus.OK));
+        assertThat(response.getBody()).hasSize(2);
+        assertThat(mapToTitles(response.getBody())).contains("Hobbit");
+    }
 }
